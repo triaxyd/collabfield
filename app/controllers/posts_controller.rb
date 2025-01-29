e4e9501ea8c75c588212ac9301e7076c74ccb59c
despkontopoulou <despkontopoulou@gmail.com>
2025-01-29 17:34:37 +0200

    new post form init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
   def show
     @post = Post.find(params[:id])
   end
@@ -14,8 +15,34 @@ class PostsController < ApplicationController
   def team
     posts_for_branch(params[:action])
   end
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def redirect_if_not_signed_in
+    redirect_to root_path if !user_signed_in?
+  end
+
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
 
   private
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+          .merge(user_id: current_user.id)
+  end
 
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
@@ -27,20 +54,10 @@ class PostsController < ApplicationController
   end
 
   def get_posts
-    branch = params[:action]
-    search = params[:search]
-    category = params[:category]
-
-    if category.blank? && search.blank?
-      posts = Post.by_branch(branch).all
-    elsif category.blank? && search.present?
-      posts = Post.by_branch(branch).search(search)
-    elsif category.present? && search.blank?
-      posts = Post.by_category(branch, category)
-    elsif category.present? && search.present?
-      posts = Post.by_category(branch, category).search(search)
-    else
-    end
+    PostsForBranchService.new({
+                                search: params[:search],
+                                category: params[:category],
+                                branch: params[:action]
+                              }).call
   end
-
 end
