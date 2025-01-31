8ff7b149eeaab931cacb871a3410a3f70a9412bd
despkontopoulou <despkontopoulou@gmail.com>
2025-01-31 20:09:11 +0200

    add contact form in posts

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,14 @@ Rails.application.routes.draw do
       get "team"
     end
   end
+
+  namespace :private do
+    resources :conversations, only: [:create] do
+      member do
+        post :close
+        post :open
+      end
+    end
+    resources :messages, only: [:index, :create]
+  end
 end
